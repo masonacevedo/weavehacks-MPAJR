2ac606bdbf57009e89f6dde4aed3ffbef40b0953
JeffRisberg <j.s.risberg@gmail.com>
2025-07-12 20:18:52 -0700

    converted pages from regular html to react jsx

--- front-end/app/layout.tsx
+++ front-end/app/layout.tsx
@@ -23,11 +23,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-    <head>
-      <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;700&display=swap"
-            rel="stylesheet"/>
-    </head>
-    <body className={`${geistSans.variable} ${geistMono.variable}`}>
+    <body>
     {children}
     </body>
     </html>
